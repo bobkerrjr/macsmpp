da3a3d50f74ce9615d23ce60fe590572f6284d9d
Bob Kerr <bobkerrjr@yahoo.com.br>
2018-10-22 00:12:42 -0300

    Initial Commit

---
+++ macsmpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  macsmpp
+//
+//  Created by Bob Kerr on 22/10/18.
+//  Copyright © 2018 Bob Kerr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
